c871e194d6fd785ed7570510869b1ce629b97b8f
vaibhav <vaibhavpuri32@gmail.com>
2020-06-10 22:46:19 +0530

    first commit

---
+++ recommender/helper/run.sh
@@ -0,0 +1,4 @@
+python3 /home/animesh/WTA/movie_recommendation/recommender/trainer/test.py
+g++ /home/animesh/WTA/movie_recommendation/recommender/trainer/process.cpp
+chmod 755 /home/animesh/WTA/movie_recommendation/recommender/trainer/a.out
+/home/animesh/WTA/movie_recommendation/recommender/trainer/a.out
